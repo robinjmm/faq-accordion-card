ede809a0cc765e5dcff95ac3d02da57dc35f327b
J.M. Robin <johnmichaelrobin@gmail.com>
2022-05-13 19:58:02 +0800

    Smooth out transitions

--- js/index.js
+++ js/index.js
@@ -1,11 +1,22 @@
 const faq = document.querySelector(".card__faq");
 
+
+
 faq.addEventListener("click", function (event) {
     let question = event.target;
 
     if (event.target.nodeName === "H3") {
+
         question.classList.toggle("active");
+
+        // target the hidden text below the question
         let text = event.target.parentElement.nextElementSibling;
         text.classList.toggle("hidden");
+        text.classList.toggle("appear");
+
+
+        // target the arrow icons and flip them upside down
+        let arrow = question.nextElementSibling;
+        arrow.classList.toggle("rotate-180");
     }
 })
