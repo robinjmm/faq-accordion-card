39e4c6b794006fceac73dde3ff5ec0f8ac0cd5dd
J.M. Robin <johnmichaelrobin@gmail.com>
2022-10-23 00:30:24 +0800

    Add utility selectors to postcss safelist

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,8 @@ module.exports = {
     plugins: [
         require("@fullhuman/postcss-purgecss")({
             content: ["src/index.html"],
-            variables: true
+            variables: true,
+            safelist: ["expand", "rotate", "bold"]
         }),
     ]
 }
