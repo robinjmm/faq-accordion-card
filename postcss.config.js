78ad95ff489ccb0b836fd1ecf08f2e7819ea57e3
J.M. Robin <johnmichaelrobin@gmail.com>
2022-11-07 01:29:01 +0800

    Replace postcss.config with .postcssrc file for parcel caching

--- postcss.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    plugins: [
-        require("@fullhuman/postcss-purgecss")({
-            content: ["src/index.html"],
-            variables: true,
-            safelist: ["expand", "rotate", "bold"]
-        }),
-    ]
-}
